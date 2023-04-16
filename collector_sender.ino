ce4e914cc65074cbae201eedfeacb68c567a0f4d
temirkhan <temirkhan.samurkhanov@mail.ru>
2023-04-16 18:09:14 +0300

    Implemented sensor operation on one I2S bus, also added a buzzer that becomes active when the carbon monoxide value rises

--- collector_sender.ino
+++ collector_sender.ino
@@ -1,18 +1,18 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 #include <Wire.h>
-#include <SPI.h>
 #include <Adafruit_BMP280.h>
 #include <Adafruit_AHTX0.h>
 #include <Adafruit_Sensor.h>
 #include <WiFiClientSecureBearSSL.h>
+#include <Ticker.h>
 
+Ticker ticker_buzzer;
+bool state_timer_buzzer = false;
 
-#define BMP_CS   15
-
-
-Adafruit_BMP280 bmp(BMP_CS);
+Adafruit_BMP280 bmp;
 Adafruit_AHTX0 aht;
+const int Buzzer = D7; 
 
 const char* ssid = "";
 const char* password = "";
@@ -20,19 +20,17 @@ const char* server_host = "";
 const char* server_port = "";
 const char* access_token = "";
 
+
 void setup(){
-  if (!bmp.begin()) {
+  pinMode (Buzzer, OUTPUT);
+
+  if (! aht.begin()) {
     while (1) delay(10);
-    
   }
 
-  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
-                  Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
-                  Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
-                  Adafruit_BMP280::FILTER_X16,      /* Filtering. */
-                  Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
-
-  aht.begin();
+  if (!bmp.begin(0x76)) {
+    while (1) delay(10);
+  }
 
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
@@ -42,23 +40,46 @@ void setup(){
   }
 }
 
+
 void loop(){
   int sensorValue = analogRead(A0); 
   sensors_event_t humidity, temp;
   aht.getEvent(&humidity, &temp);
 
-  std::unique_ptr<BearSSL::WiFiClientSecure>client(new BearSSL::WiFiClientSecure);
+  if(sensorValue > 700 && !state_timer_buzzer){
+    ticker_buzzer.attach(0.1, handler_activate_buzzer);
+    state_timer_buzzer = true;
+  }
+  else if(sensorValue <= 700){
+    ticker_buzzer.detach();
+    digitalWrite (Buzzer, LOW);
+    state_timer_buzzer = false;
+  }
+
+  send_data(bmp.readTemperature(), bmp.readPressure(), sensorValue, humidity.unitless_percent);
+  delay(2000);
+}
+
+
+void handler_activate_buzzer(){
+  digitalWrite (Buzzer, !(digitalRead(Buzzer)));
+}
+
+
+void send_data(double temperature, double pressure, double carbonMonoxide, double humidity){
+ std::unique_ptr<BearSSL::WiFiClientSecure>client(new BearSSL::WiFiClientSecure);
   client->setInsecure();
 
   if (WiFi.status() == WL_CONNECTED) {
 
-    String url = "https://" + server_host + ":" + server_port + "/#" + "access_token=" + access_token + "&temperature=" + bmp.readTemperature() + "&pressure=" + bmp.readPressure() + "&carbonMonoxide=" +  sensorValue + "&humidity=" + humidity.unitless_percent;
+    String url = "https://" +  String(server_host) + ":" + server_port + "/#" + "access_token=" + access_token + "&temperature=" + temperature + "&pressure=" + pressure * 0.75 + "&carbonMonoxide=" +  carbonMonoxide + "&humidity=" + humidity;
     HTTPClient https;
 
-    https.begin(*client, url);  
+    https.begin(*client, url); 
     int httpCode = https.GET();
+
+    Serial.println(httpCode);
 
     https.end();
   }
-
-  del
+}
